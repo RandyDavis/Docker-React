e3b1f6163126b7ff8de06dd7d3d0e39369f309e0
Randy Davis <randy_davis@outlook.com>
2020-02-05 15:03:27 -0600

    Changed app text again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo there!
+          I was changed on the feature branch called 'CI/CD'.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch called 'CI/CD'.
+          Learn React
         </a>
       </header>
     </div>
